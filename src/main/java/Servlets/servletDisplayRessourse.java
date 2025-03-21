c5eec700f0a477334aedd108e4ed84d05e53a7ff
hajar zerhouni <hajar.zerhounipr@gmail.com>
2025-03-21 01:22:03 +0000

    afficer un ressource displayRessource() dao

--- src/main/java/Servlets/servletDisplayRessourse.java
+++ src/main/java/Servlets/servletDisplayRessourse.java
@@ -1,4 +1,23 @@
 package Servlets;
 
-public class servletDisplayRessourse {
+import Models.Ressource;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet("/displayRessource")
+
+public class servletDisplayRessourse extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<Ressource> ressources = new ArrayList<>();
+        req.setAttribute("ressources", ressources);
+        req.getRequestDispatcher("displayRessource").forward(req , resp);
+    }
 }
